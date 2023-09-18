e99ca15f81596daa877a9e442e4b2cd72fe98aa3
Anton <anton.omelianenko533@gmail.com>
2023-09-18 20:43:30 +0300

    implement search results - part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,11 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 // import 'core-js/stable';
 // import 'regenerator-runtime/runtime';
@@ -30,12 +35,15 @@ async function controlRecipes() {
 
 async function controlSearchResults() {
   try {
+    resultsView.renderSpinner();
+
     const query = searchView.getQuery();
     if (!query) return;
 
     await model.loadSearchResults(query);
 
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
