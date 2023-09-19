a672f629f599cdc986288e260de096ba97fb7c98
Anton <anton.omelianenko533@gmail.com>
2023-09-19 18:35:58 +0300

    implement pagination - part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -36,20 +37,29 @@ async function controlRecipes() {
 async function controlSearchResults() {
   try {
     resultsView.renderSpinner();
-
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
-
+    // 2) Load search results
     await model.loadSearchResults(query);
-
+    // 3) Render results
     // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 }
 
+function controlPagination(goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  // 4) Render initial pagination buttons
+  paginationView.render(model.state.search);
+}
+
 (function init() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 })();
